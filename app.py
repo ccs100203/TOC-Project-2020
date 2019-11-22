c02069cec86bfe1c40efcbfbf110c5e6e9e65009
ccs100203 <ccs100203@gmail.com>
2019-11-22 23:10:26 +0800

    test

--- app.py
+++ app.py
@@ -116,5 +116,7 @@ def show_fsm():
 
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # port = os.environ.get("PORT", 8000)
+    # app.run(host="0.0.0.0", port=port, debug=True)
+    PORT = os.environ['PORT']
+    app.run(host="0.0.0.0", port=PORT, debug=True)
